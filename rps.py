e4826670d1c4f7350b8b94032676dc30037d8172
amir <amirmojtahedi4144@gmail.com>
2024-09-30 12:24:51 +0330

    commit2
    this is my second repo

---
+++ rps.py
@@ -0,0 +1,17 @@
+print("rock" and   "paper" and   "scissorse")
+player1 = input("player1 plz  make your move : ")
+player2 = input("player2 plz make your move: ")
+if player1 == "rock" and player2 == "scissors":
+    print ("player 1 wins")
+elif player1 == "rock" and player2 == "paper":
+    print("player 2 win ")
+elif player1 == "paper" and player2 == "rock":
+    print("player 1 win ")
+elif player1 == "paper" and player2 == "scissors":
+    print("player 2 wins")
+elif player1 == "scissors" and player == "paper":
+    print("player1 wins")
+elif player1 == "scissors" and player2 == "rock":
+    print("player2 wins")
+else:
+    ("moshkeli pish amade")
