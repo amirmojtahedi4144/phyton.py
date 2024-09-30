4b6a4e58a4db012cf85dddd099c4e2d59319940e
amir <amirmojtahedi4144@gmail.com>
2024-09-30 12:23:48 +0330

    commit1
    this my first repo

---
+++ halghe4.py
@@ -0,0 +1,51 @@
+flower_group = ["roze" , "lale" , "narges" , "nastaran"]
+
+for flower_group1 in flower_group:
+    print(flower_group1)
+    
+##################################################################
+city_group = ["sari" , "qaemshahr" , "babol" , "zahedan"]
+
+for city_group1 in city_group:
+    print(city_group1)
+    
+###################################################################
+country_group = ["iran" , "araq" , "usa" , "africa"]
+
+for country_group1 in country_group:
+    print(city_group1)
+    
+#####################################################################
+car_group = ["pars" , "peykan" , "rana" , "samand"]
+
+for car_group1 in car_group:
+    print(car_group1)
+    
+######################################################################
+exercise_group = ["football" , "basketball" , "vollyball" , "tennis"]
+
+for exercise_group1 in exercise_group:
+    print(exercise_group1)flower_group = ["roze" , "lale" , "narges" , "nastaran"]
+
+for flower_group1 in flower_group:
+    print(flower_group1)
+    
+##################################################################
+city_group = ["sari" , "qaemshahr" , "babol" , "zahedan"]
+
+for city_group1 in city_group:
+    print(city_group1)
+    
+###################################################################
+country_group = ["iran" , "araq" , "usa" , "africa"]
+
+for country_group1 in country_group:
+    print(city_group1)
+    
+#####################################################################
+car_group = ["pars" , "peykan" , "rana" , "samand"]
+
+for car_group1 in car_group:
+    print(car_group1)
+    
+##################################################################
